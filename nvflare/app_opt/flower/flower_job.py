d5279b8e0335962319d7359e15bf7683478329ac
Yan Cheng <yanc@nvidia.com>
2024-08-27 17:48:59 -0400

    support metrics streaming in FlowerJob

--- nvflare/app_opt/flower/flower_job.py
+++ nvflare/app_opt/flower/flower_job.py
@@ -15,6 +15,12 @@ import os.path
 from typing import List, Optional
 
 from nvflare.app_common.tie.defs import Constant
+from nvflare.app_common.widgets.external_configurator import ExternalConfigurator
+from nvflare.app_common.widgets.metric_relay import MetricRelay
+from nvflare.app_common.widgets.streaming import AnalyticsReceiver
+from nvflare.app_opt.tracking.tb.tb_receiver import TBAnalyticsReceiver
+from nvflare.fuel.utils.pipe.cell_pipe import CellPipe
+from nvflare.fuel.utils.validation_utils import check_object_type
 from nvflare.job_config.api import FedJob
 
 from .controller import FlowerController
@@ -38,6 +44,8 @@ class FlowerJob(FedJob):
         per_msg_timeout=10.0,
         tx_timeout=100.0,
         client_shutdown_timeout=5.0,
+        stream_metrics=False,
+        analytics_receiver=None,
     ):
         """
         Flower Job.
@@ -57,6 +65,8 @@ class FlowerJob(FedJob):
             per_msg_timeout (float, optional): Timeout for receiving individual messages. Defaults to 10.0 seconds.
             tx_timeout (float, optional): Timeout for transmitting data. Defaults to 100.0 seconds.
             client_shutdown_timeout (float, optional): Timeout for client shutdown. Defaults to 5.0 seconds.
+            stream_metrics (bool, optional): Whether to stream metrics from Flower client to Flare
+            analytics_receiver (AnalyticsReceiver, optional): the AnalyticsReceiver to use to process received metrics.
         """
         if not os.path.isdir(flower_content):
             raise ValueError(f"{flower_content} is not a valid directory")
@@ -82,3 +92,39 @@ class FlowerJob(FedJob):
         )
         self.to_clients(executor)
         self.to_clients(obj=flower_content)
+
+        if not stream_metrics:
+            return
+
+        # add required components for metrics streaming
+        # server side - need analytics_receiver
+        if analytics_receiver:
+            check_object_type("analytics_receiver", analytics_receiver, AnalyticsReceiver)
+        else:
+            analytics_receiver = TBAnalyticsReceiver(events=["fed.analytix_log_stats"])
+
+        self.to_server(analytics_receiver, "analytics_receiver")
+
+        # client side
+        # cell pipe
+        cell_pipe = CellPipe(
+            mode="PASSIVE",
+            site_name="{SITE_NAME}",
+            token="{JOB_ID}",
+            root_url="{ROOT_URL}",
+            secure_mode="{SECURE_MODE}",
+            workspace_dir="{WORKSPACE}",
+        )
+        pipe_id = self.to_clients(cell_pipe, "metrics_pipe")
+
+        metric_relay = MetricRelay(
+            pipe_id=pipe_id,
+            event_type="fed.analytix_log_stats",
+            read_interval=0.1,
+            heartbeat_timeout=0,
+            fed_event=True,
+        )
+
+        relay_id = self.to_clients(metric_relay, "metric_relay")
+        conf = ExternalConfigurator(component_ids=[relay_id])
+        self.to_clients(conf, "client_api_config_preparer")
